6a998f57a3494787d2134eaca7bbf8ea1d234b2d
mrice <mricekanal@gmail.com>
2023-03-14 19:10:32 +0300

    14.03.23

--- data_base/sqldb.py
+++ data_base/sqldb.py
@@ -8,17 +8,17 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('DATA BASE WAS CONNECTED SUCCSESSFUL')
-    base.execute("CREATE TABLE IF NOT EXISTS {}(name TEXT, age TEXT, contact TEXT PRIMARY KEY, information TEXT)".format("menu"))
+    base.execute("CREATE TABLE IF NOT EXISTS {}(name TEXT, age TEXT, contact TEXT PRIMARY KEY, information TEXT, photo TEXT)".format("menu"))
     base.commit()
 
 async def insert_data_command(state):
     async with state.proxy() as data:
-        cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
+        cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?, ?)', tuple(data.values()))
         base.commit()
 
 async def sql_read(ID):
     for ret in cur.execute('SELECT * FROM menu').fetchall():
-        await bot.send_message(ID, f'Имя: {ret[0]}\nВозраст: {ret[1]}\nКонтакт: {ret[2]}\nИнформация: {ret[-1]}')
+        await bot.send_photo(ID, ret[-1], f'Имя: {ret[0]}\nВозраст: {ret[1]}\nКонтакт: {ret[2]}\nИнформация: {ret[3]}')
 
 async def sql_read2():
     return cur.execute('SELECT * FROM menu').fetchall()
--- handlers/admin.py
+++ handlers/admin.py
@@ -22,6 +22,7 @@ class FSMAdmin(StatesGroup):
     age = State()
     contact = State()
     information = State()
+    photo = State()
 
 #Проверка на модера
 async def make_changes_command(msg : types.Message):
@@ -72,6 +73,12 @@ async def load_contact(msg : types.Message, state : FSMContext):
 async def load_information(msg : types.Message, state : FSMContext):
     async with state.proxy() as data:
         data['information'] = msg.text
+    await FSMAdmin.next()
+    await msg.reply('Пришлите ваше фото, пожалуйста')
+
+async def load_photo(msg : types.Message, state : FSMContext):
+    async with state.proxy() as data:
+        data['photo'] = msg.photo[-1].file_id
     try:
         await sqldb.insert_data_command(state)
         print("New data was added to database")
@@ -79,9 +86,11 @@ async def load_information(msg : types.Message, state : FSMContext):
             await bot.send_message(chat_id=ID, text=f"Босс у вас новый клиент")
             await sqldb.sql_read(ID)
         except:
-            await bot.send_message(msg.from_user.id, f"Данные успешно заполнены, подождите пока с вами не свяжутся", reply_markup=client_kb.client_kb1)
+            await bot.send_message(msg.from_user.id, f"Данные успешно заполнены, подождите пока с вами не свяжутся",
+                                       reply_markup=client_kb.client_kb1)
     except:
-        await bot.send_message(msg.from_user.id, f"Произошла ошибка добавления в базу данных", reply_markup=client_kb.client_kb1)
+        await bot.send_message(msg.from_user.id, f"Произошла ошибка добавления в базу данных",
+                                reply_markup=client_kb.client_kb1)
 
     await state.finish()
 
@@ -96,7 +105,7 @@ async def delete_item(message : types.Message):
     if message.from_user.id == ID:
         read = await sqldb.sql_read2()
         for ret in read:
-            await bot.send_message(ID, f'Имя: {ret[0]}\nВозраст: {ret[1]}\nКонтакт: {ret[2]}\nИнформация: {ret[-1]}', reply_markup=InlineKeyboardMarkup().\
+            await bot.send_photo(ID, ret[-1], f'Имя: {ret[0]}\nВозраст: {ret[1]}\nКонтакт: {ret[2]}\nИнформация: {ret[3]}', reply_markup=InlineKeyboardMarkup().\
                                    add(InlineKeyboardButton(f"Удалить {ret[0]}", callback_data=f"del {ret[0]}")))
 #Выход из админки
 async def leave_admin(msg : types.Message):
@@ -111,6 +120,7 @@ def register_admin_handlers(dp : Dispatcher):
     dp.register_message_handler(load_age, state=FSMAdmin.age)
     dp.register_message_handler(load_contact, state=FSMAdmin.contact)
     dp.register_message_handler(load_information, state=FSMAdmin.information)
+    dp.register_message_handler(load_photo,content_types=['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(make_changes_command, commands=['moder'], is_chat_admin=True)
     dp.register_message_handler(list_of_names, commands=['Список'])
     dp.register_message_handler(delete_item, commands=['Удалить'])
